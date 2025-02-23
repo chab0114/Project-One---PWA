c8c7e17045c95332604938c7c0134f5ad434c5b6
chab0114 <chab0114@algonquinlive.com>
2025-02-22 22:07:45 -0500

    WIP: PWA manifest and icons

--- sw.js
+++ sw.js
@@ -13,7 +13,13 @@ const ASSETS_TO_CACHE = [
     '/index.html',
     '/css/main.css',
     '/js/main.js',
+    '/manifest.json',
     '/assets/images/placeholder.jpg',
+    '/assets/icons/favicon-16x16.png',
+    '/assets/icons/favicon-32x32.png',
+    '/assets/icons/favicon-96x96.png',
+    '/assets/icons/favicon-192x192.png',
+    '/assets/icons/favicon-512x512.png',
     'https://fonts.googleapis.com/css2?family=Playfair+Display+SC:wght@700&family=SF+Pro+Text:wght@400;500;600&family=DM+Sans:wght@400;600;700&display=swap',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
